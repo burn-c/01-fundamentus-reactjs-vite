d0cf73e64693303a11fef22d1b8ba5789d6d9bc8
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-25 23:33:05 -0300

    feat(comp): adds Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Header from './components/Header'
 import Post from './components/Post'
+import Sidebar from './components/Sidebar'
+
+import styles from './App.module.css'
 
 function App() {
  return (
   <>
       <Header/>
       
-      <Post 
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+
+        <Post 
         author="Carlos Oliveira"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
       />
@@ -20,6 +28,10 @@ function App() {
         author="Ciclano Oliveira"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
       />
+
+        </main>
+      </div>
+      
     </>
  )
 }
