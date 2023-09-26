6c73536c8daa24558c455e87dc7d7a6dda1464f9
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-25 23:53:36 -0300

    style: adds profile styles on Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export default function Sidebar() {
@@ -9,12 +11,15 @@ export default function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/54965836?v=4"/>
+
                 <strong>Carlos Oliveira</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
