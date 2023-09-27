bf7f6e18cacb4289db765683a1de6b635ade4ab0
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-26 23:48:49 -0300

    feat(comp): adds new  Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,28 @@
 /* eslint-disable react/prop-types */
-export default function Post({author, content}) {
+
+import styles from './Post.module.css'
+
+export default function Post() {
     return (
-    <>
-        <strong>{author}</strong>
-        <p>{content}</p>
-    </>
+    <article className={styles.post}>
+        <header>
+            <div className={styles.author}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/54965836?v=4" />
+                <div className={styles.authorInfo}>
+                    <strong>Carlos Oliveira</strong>
+                    <span>Web Developer</span>
+                </div>
+            </div>
+
+            <time title="11 de Maio às 08:13h" dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+        </header>
+
+        <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+            <p><a href="">👉 jane.design/doctorcare</a></p> 
+            <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        </div>
+    </article>
     )
 }
