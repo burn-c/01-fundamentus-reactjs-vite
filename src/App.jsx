927772575316db658a3b6e458031ee990673fd65
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-29 01:24:58 -0300

    feat(comp): mock post data and adds props

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,59 @@ import Sidebar from './components/Sidebar'
 
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/burn-c.png',
+      name: 'Carlos Oliveira',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉 jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-09-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'João Roberto',
+      role: 'Backend Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉 jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-09-16 20:00:00')
+  },
+]
+
 function App() {
  return (
   <>
-      <Header/>
-      
-      <div className={styles.wrapper}>
-        <Sidebar />
+    <Header/>  
+    
+    <div className={styles.wrapper}>
+      <Sidebar />
         
-        <main>
-
-        <Post 
-        author="Carlos Oliveira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
-      />
-
-      <Post
-        author="Fulano Oliveira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
-      />
-      
-      <Post
-        author="Ciclano Oliveira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
-      />
-
-        </main>
-      </div>
-      
-    </>
+      <main>       
+        {
+          posts?.map(post =>
+            <Post 
+              key={post?.id}
+              author={post?.author}
+              content={post?.content}
+              publishedAt={post?.publishedAt}
+            />
+          )
+        }
+      </main>
+    </div>  
+  </>
  )
 }
 
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,34 +3,34 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import Avatar from './Avatar'
 
-export function Comment() {
-    return (
-        <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/burn-c.png" alt="Profile image" />
+export default function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/burn-c.png" alt="Profile image" />
 
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Carlos Oliveira</strong>
-                            <time title="11 de Maio às 08:13h" dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
-                        </div>
+    <div className={styles.commentBox}>
+      <div className={styles.commentContent}>
+        <header>
+          <div className={styles.authorAndTime}>
+            <strong>Carlos Oliveira</strong>
+            <time title="11 de Maio às 08:13h" dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>            
+          </div>
 
-                        <button title='Deletar comentário'>
-                            <Trash size={24}/>
-                        </button>
-                    </header>
+          <button title='Deletar comentário'>
+            <Trash size={24}/>
+          </button>
+        </header>
 
-                    <p>Muito bom, parabéns!!!</p>
-                </div>
+        <p>Muito bom, parabéns!!!</p>
+      </div>
 
-                <footer>
-                    <button>
-                        <ThumbsUp/>
-                    Aplaudir <span>20</span>
-                    </button>
-                </footer>
-            </div>
-        </div>
-    )
+          <footer>
+              <button>
+                  <ThumbsUp/>
+              Aplaudir <span>20</span>
+              </button>
+          </footer>
+      </div>
+  </div>
+  )
 }
